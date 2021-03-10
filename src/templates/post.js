f824af1af9fea5b5a9b2b6369474be107efe6af6
kojima <kjima18@gmail.com>
2021-03-10 11:15:00 +0900

    投稿の最下部にSNSシェア用アイコン設置

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,12 +2,39 @@ import React from "react"
 import "../styles/post.css"
 import Layout from "../components/layout";
 import SEO from "../components/seo"
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  LineShareButton,
+  LineIcon,
+} from 'react-share';
 
-const Post = ({ pageContext }) => {
+const SNSSection = ({ title, shareUrl }) => {
+  return(
+    <div>
+      <TwitterShareButton title={title} url={shareUrl} className="mr">
+        <TwitterIcon size={40} round />
+      </TwitterShareButton>
+
+      <FacebookShareButton url={shareUrl} className="mr">
+        <FacebookIcon size={40} round />
+      </FacebookShareButton>
+
+      <LineShareButton url={shareUrl}>
+        <LineIcon size={40} round />
+      </LineShareButton>
+    </div>
+  )
+}
+
+const Post = ({ location, pageContext }) => {
   const { title, createdAt, image, tags } = pageContext.post;
   const description = pageContext.post.description.description;
   const body = pageContext.post.body.childMarkdownRemark.html;
   const tagLists = pageContext.tags;
+  const shareUrl = location.href;
 
   return(
     <Layout tags={tagLists}>
@@ -23,6 +50,7 @@ const Post = ({ pageContext }) => {
       </div>
       <img src={image.file.url} className="post-image" alt="post-cover"></img>
       <div dangerouslySetInnerHTML={{ __html: body }} className="post-body" />
+      <SNSSection title={title} shareUrl={shareUrl} />
     </Layout>
   )
 }
