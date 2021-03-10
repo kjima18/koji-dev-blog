97611149ae1a98bb4abe313f18ab348320e78a54
kojima <kjima18@gmail.com>
2021-03-10 11:34:22 +0900

    サイドバーにアイコン設置

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,6 +2,26 @@ import React from 'react';
 import { Link } from 'gatsby';
 import '../styles/sidebar.css'
 import Account from '../../static/account.jpg';
+import {
+  FacebookIcon,
+  TwitterIcon
+} from 'react-share';
+
+const SNSLink = () => {
+  const TwitterUrl = 'https://twitter.com/prokoji18'
+  const FacebookUrl = 'https://www.facebook.com/profile.php?id=100006424912340'
+  return(
+    <div>
+      <Link to={TwitterUrl} target="_blank" className='mr'>
+        <TwitterIcon size={25} round />
+      </Link>
+
+      <Link to={FacebookUrl} target="_blank">
+        <FacebookIcon size={25} round />
+      </Link>
+    </div>
+  )
+}
 
 const Sidebar = ({ tags }) => {
   return(
@@ -12,6 +32,9 @@ const Sidebar = ({ tags }) => {
         </div>
         <h2 className='title'>Koji Dev Blog</h2>
         <h5 className='description'>仙台でリモート&スクラムを実践するエンジニアの技術ブログ</h5>
+        <div className='sns-link'>
+          <SNSLink/>
+        </div>
       </div>
       <div className='sidebar__item'>
         <h3 className='sidebar-title'>タグ</h3>
